458777c5fc37d9ef4210d416500b8fd316bdebdb
Paul Perkins <paul@paulperkins.net>
2015-08-28 19:02:29 +1000

    Added bulk custom actions.

--- class-bimbler-woofoo.php
+++ class-bimbler-woofoo.php
@@ -72,9 +72,179 @@ class Bimbler_WooFoo {
 
 			// Turn off breadcrumbs on Woo pages.			
 			add_action( 'init', array ($this, 'remove_wc_breadcrumbs'));
-
+			
+			// Add hook for custom statuses on order page.
+			add_action('admin_footer-edit.php', array ($this, 'custom_bulk_admin_footer'));
+			
+			// Process bulk updates.
+			add_action('load-edit.php', array ($this, 'custom_bulk_action')); 
         	
 		} // End constructor.
+	
+	
+	
+	
+		/**
+		* Adds a note (comment) to the order
+		*
+		* @access public
+		* @param string $note Note to add
+		* @param int $is_customer_note (default: 0) Is this a note for the customer?
+		* @return id Comment ID
+		*
+		* *file is class-wp-order.php*
+		*/
+		public function add_order_note( $post_id, $note, $is_customer_note = 0 ) {
+		
+			$is_customer_note = intval( $is_customer_note );
+		
+			if ( is_user_logged_in() && current_user_can( 'manage_woocommerce' ) ) {
+				$user                 = get_user_by( 'id', get_current_user_id() );
+				$comment_author       = $user->display_name;
+				$comment_author_email = $user->user_email;
+			} else {
+				$comment_author       = __( 'WooCommerce', 'woocommerce' );
+				$comment_author_email = strtolower( __( 'WooCommerce', 'woocommerce' ) ) . '@';
+				$comment_author_email .= isset( $_SERVER['HTTP_HOST'] ) ? str_replace( 'www.', '', $_SERVER['HTTP_HOST'] ) : 'noreply.com';
+				$comment_author_email = sanitize_email( $comment_author_email );
+			}
+		
+			$comment_post_ID        = $post_id;
+			$comment_author_url     = '';
+			$comment_content        = $note;
+			$comment_agent          = 'WooCommerce';
+			$comment_type           = 'order_note';
+			$comment_parent         = 0;
+			$comment_approved       = 1;
+			$commentdata            = apply_filters( 'woocommerce_new_order_note_data', compact( 'comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_agent', 'comment_type', 'comment_parent', 'comment_approved' ), array( 'order_id' => $this->id, 'is_customer_note' => $is_customer_note ) );
+		
+			$comment_id = wp_insert_comment( $commentdata );
+		
+			add_comment_meta( $comment_id, 'is_customer_note', $is_customer_note );
+		
+			if ( $is_customer_note )
+				do_action( 'woocommerce_new_customer_note', array( 'order_id' => $this->id, 'customer_note' => $note ) );
+		
+			return $comment_id;
+		}
+			
+		// Add custom statuses on order page.
+		function custom_bulk_admin_footer() {
+ 
+			global $post_type;
+			
+			if($post_type == 'shop_order') {
+				?>
+				<script type="text/javascript">
+					jQuery(document).ready(function() {
+						jQuery('<option>').val('wc-bimbler-paid').text('<?php _e('Mark paid')?>').appendTo("select[name='action']");
+						jQuery('<option>').val('wc-bimbler-paid').text('<?php _e('Mark paid')?>').appendTo("select[name='action2']");
+						jQuery('<option>').val('wc-bimbler-ordered').text('<?php _e('Mark ordered')?>').appendTo("select[name='action']");
+						jQuery('<option>').val('wc-bimbler-ordered').text('<?php _e('Mark ordered')?>').appendTo("select[name='action2']");
+						jQuery('<option>').val('wc-bimbler-arrived').text('<?php _e('Mark arrived')?>').appendTo("select[name='action']");
+						jQuery('<option>').val('wc-bimbler-arrived').text('<?php _e('Mark arrived')?>').appendTo("select[name='action2']");
+					});
+				</script>
+				<?php
+			} 
+		}
+		
+		function update_post_status ($post_id, $new_status, $new_status_text) {
+			
+			$post_object = get_post ($post_id);
+			
+			error_log ('Updating order ID ' . $post_id . ' status from ' . $post_object->post_status . ' to ' . $new_status);
+			
+			$post_object->post_status = $new_status;
+			
+			wp_update_post ($post_object);
+			
+			$this->add_order_note ($post_id, 'Order status changed to ' . $new_status_text . '.');
+			//error_log ('Post:' .  json_encode ($post_object));
+		}
+		
+		// Process custom bulk action.
+		function custom_bulk_action () {
+			
+			$wp_list_table = _get_list_table('WP_Posts_List_Table');
+			
+			$action = $wp_list_table->current_action();
+			
+			if (empty ($action)) {
+				return;
+			}
+			
+			//error_log ('Bulk action is ' . $action);
+			
+			check_admin_referer('bulk-posts');
+
+			$post_ids = $_GET['post'];
+			
+			$sendback = 'post_type=shop_order';
+			
+			$sendback = remove_query_arg( array('ordered', 'arrived', 'paid', 'ids'), wp_get_referer() );
+			
+			if ( ! $sendback ) {				
+				$sendback = admin_url( "edit.php?post_type=$post_type" );
+			}
+			
+			switch ($action) {
+				case 'wc-bimbler-paid':
+				
+					$new_status = 'Paid';
+					
+					$paid = 0;
+					
+					foreach( $post_ids as $post_id ) {
+						$this->update_post_status ($post_id, $action, $new_status);
+							
+						$paid++;
+					}
+									
+					$sendback = add_query_arg( array('paid' => $paid, 'ids' => join(',', $post_ids) ), $sendback );
+
+					break;
+					
+				case 'wc-bimbler-ordered':
+				
+					$new_status = 'Ordered';
+
+					$ordered = 0;
+					
+					foreach( $post_ids as $post_id ) {
+						$this->update_post_status ($post_id, $action, $new_status);
+							
+						$ordered++;
+					}
+									
+					$sendback = add_query_arg( array('ordered' => $ordered, 'ids' => join(',', $post_ids) ), $sendback );
+
+					break;
+					
+				case 'wc-bimbler-arrived':
+					$new_status = 'Arrived';
+				
+					$arrived = 0;
+					
+					foreach( $post_ids as $post_id ) {
+						$this->update_post_status ($post_id, $action, $new_status);
+							
+						$arrived++;
+					}
+									
+					$sendback = add_query_arg( array('arrived' => $arrived, 'ids' => join(',', $post_ids) ), $sendback );
+
+					break;
+					
+					
+				default:
+					return;
+			}
+			
+			wp_redirect($sendback);
+			
+			exit ();
+		}
 	
 		function register_bimbler_order_statuses() {
 			
